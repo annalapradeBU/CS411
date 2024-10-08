9c2737c195720c653d7dbc87ee5235575d4dc560
Anna LaPrade <annalaprade@crc-dot1x-nat-10-239-149-77.bu.edu>
2024-10-07 23:40:15 -0400

    HW 2

--- HW3_Design/wildlife_tracker/animal_management/__init__.py
+++ HW3_Design/wildlife_tracker/animal_management/__init__.py
@@ -1,4 +1,2 @@
 # __init__.py
 
-from typing import Any, Optional
-from wildlife_tracker.animal_management.animal import Animal
--- HW3_Design/wildlife_tracker/habitat_management/__init__.py
+++ HW3_Design/wildlife_tracker/habitat_management/__init__.py
@@ -1,5 +1,2 @@
 # __init__.py
 
-from typing import Any, List, Optional
-from wildlife_tracker.habitat_management.habitat import Habitat
-from wildlife_tracker.animal_management.animal import Animal
